eb0cb237c634a8a54d855f76e8488e7d5c019396
mrpanda1925 <62003465+mrpanda1925@users.noreply.github.com>
2024-07-14 15:06:48 +0800

    Create Event.java

---
+++ src/main/java/com/example/eventmanagement/Event.java
@@ -0,0 +1,21 @@
+package com.example.eventmanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String date;
+    private String location;
+    private String description;
+    private String registrationOptions;
+    private String ticketType;
+
+    // Getters and Setters
+}
